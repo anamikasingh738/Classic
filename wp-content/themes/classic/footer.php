b04f80d63effab1fc070a3f7c979dec60f597ad1
mandeep25 <mandeep.rishti@gmail.com>
2022-03-18 08:29:48 +0530

    Footer Changes

--- wp-content/themes/classic/footer.php
+++ wp-content/themes/classic/footer.php
@@ -14,5 +14,20 @@
 </div>
 
 <?php wp_footer(); ?>
+<script>
+  $(".testimonial-slider").slick({
+        dots: false,
+		arrows: true,
+		 prevArrow: "<i class='left round'><img src='<?php echo get_template_directory_uri();?>/images/right.svg'></i>",
+         nextArrow: "<i class='right round'><img src='<?php echo get_template_directory_uri();?>/images/left.svg'></i>",
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		 variableWidth: false,
+          centerMode: false,
+		
+		
+      });
+</script>
 </body>
 </html>
--- wp-content/themes/classic/template-home.php
+++ wp-content/themes/classic/template-home.php
@@ -28,18 +28,4 @@ get_header();
 
 
 <?php get_footer(); ?>
-<script>
-  $(".testimonial-slider").slick({
-        dots: false,
-		arrows: true,
-		 prevArrow: "<i class='left round'><img src='<?php echo get_template_directory_uri();?>/images/right.svg'></i>",
-         nextArrow: "<i class='right round'><img src='<?php echo get_template_directory_uri();?>/images/left.svg'></i>",
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-		 variableWidth: false,
-          centerMode: false,
-		
-		
-      });
-</script>
+
